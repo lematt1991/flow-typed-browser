7320ed2203e36665bb8375546595b105da2a2347
Matt Le <ml9951@rit.edu>
2017-07-31 14:54:46 -0400

    Adding index page to browser packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Browser from './pages/Browser'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Browser} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/pages/Browser.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import axios from 'axios'
+import 'react-select/dist/react-select.css';
+import Select from 'react-select'
+import { Button } from 'react-bootstrap'
+
+export default class Browser extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      defs : [],
+      selected : null
+    }
+  }
+
+  componentWillMount(){
+    axios.get('/defs')
+      .then(({data}) => {
+        this.setState({...this.state, defs : data.map(f => ({value : f, label : f}))})
+      })
+  }
+
+  render(){
+    return(
+      <div>
+        <div style={styles.container}>
+          <h2 style={{color : 'white'}}>
+            Flow Typed Browser
+          </h2>
+        </div>
+        <div style={{width : '100%', marginTop : 30}}>
+          <h3 className='text-center'>Select Project</h3>
+          <div  style={{margin : '0 auto', width : '70%'}}>
+            <Select
+              value={this.state.selected}
+              options={this.state.defs}
+              onChange={option => this.setState({...this.state, selected : option})}
+            />
+          </div>
+          <div  style={{display : 'flex', justifyContent : 'center', marginTop : 20}}>
+            <Button bsStyle='primary' disabled={this.state.selected == null}>
+              View Defs
+            </Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  container : {
+    height : 150, 
+    backgroundColor : 'black', 
+    margin : 0,
+    display : 'flex',
+    justifyContent : 'center',
+    alignItems : 'center'
+  }
+}
